17f8164498f3bfd7b389b38877e3260f7db75cb2
Satyajit Bhosale <125465151+satyajit5007@users.noreply.github.com>
2026-03-25 23:12:01 +0530

    Catalog Service module setup

---
+++ catalog-service/src/test/java/com/satyajit/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satyajit.bookstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(ContainersConfig.class)
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
